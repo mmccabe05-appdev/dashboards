cc9e8bb7181e74e7ab6f455b507e375679306dd5
mmccabe05 <mmccabe@chicagobooth.edu>
2022-10-24 21:42:49 +0000

    initial RCAV set up for forex pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  # require URI
+  def main_page 
+
+    render({:template => "main_page.html.erb"})
+  end
+  def forex 
+
+    render({:template => "forex.html.erb"})
+  end
+  def covid 
+
+    render({:template => "covid.html.erb"})
+  end
 end
